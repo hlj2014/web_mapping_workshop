5e9aa108ffbaaf17d95956138099d72b4a0035e8
hlj2014 <hj24@duke.edu>
2016-05-02 14:41:21 -0400

    Replaced instructor's mapId & accessToken with own from Mapbox

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,10 +2,10 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.019h7bl7';
+var mapId = 'hannahlj.019h4gl1';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+var accessToken = 'pk.eyJ1IjoiaGFubmFobGoiLCJhIjoianVVaDBGUSJ9.sj1JyjPhgx-Ndar3AZSo8g';
 
 // Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
